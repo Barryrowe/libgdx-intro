9076a7aa63683a3c29b67b00b8e914e643ab571e
Barryrowe <barryrowe@gmail.com>
2017-11-12 15:29:31 -0500

    Add logs to all input processor items
    The input processor is filled out with logs

--- core/src/com/roaringcatgames/cryptidcapers/CryptidCapersGame.java
+++ core/src/com/roaringcatgames/cryptidcapers/CryptidCapersGame.java
@@ -2,6 +2,7 @@ package com.roaringcatgames.cryptidcapers;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
@@ -11,7 +12,7 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
-public class CryptidCapersGame extends ApplicationAdapter {
+public class CryptidCapersGame extends ApplicationAdapter implements InputProcessor {
 
     private SpriteBatch batch;
     private Vector2 nessiePosition = new Vector2(257f, 3f);
@@ -29,6 +30,7 @@ public class CryptidCapersGame extends ApplicationAdapter {
 
 	@Override
 	public void create () {
+        log("CREATE FIRED");
 		batch = new SpriteBatch();
 
 		nessieStill = new TextureRegion(new Texture("nessie/still.png"));
@@ -44,6 +46,8 @@ public class CryptidCapersGame extends ApplicationAdapter {
             1f/5f,
             new Array<TextureRegion>(walkingTextures),
             Animation.PlayMode.LOOP);
+
+        Gdx.input.setInputProcessor(this);
 
         log("CREATE FIRED");
 	}
@@ -101,7 +105,7 @@ public class CryptidCapersGame extends ApplicationAdapter {
         }
     }
 
-    private void draw(){
+    private void draw() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
@@ -109,5 +113,53 @@ public class CryptidCapersGame extends ApplicationAdapter {
         batch.draw(nessieAnimatedFrame, nessiePosition.x, nessiePosition.y);
 
         batch.end();
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+	    log("Key Down: " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        log("Key Up: " + keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+	    log("Key Typed: " + character);
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+	    log("Touch Down from Pointer:" + pointer + " @ X: " + screenX + " Y: " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        log("Touch Up from Pointer:" + pointer + " @ X: " + screenX + " Y: " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        log("Touch Dragged from Pointer:" + pointer + " @ X: " + screenX + " Y: " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+	    log("Mouse Moved  @ X: " + screenX + " Y: " + screenY);
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+	    log("Scrolled by: " + amount);
+        return false;
     }
 }
